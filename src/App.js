a5863607d6655cb0aa31e660da8e3c7d0ac96e8a
shabeebleo <shabeebpv0786@gmail.com>
2023-10-09 14:15:17 +0530

    home page design

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">
+          MyBlog
+        </a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+      <div className="post">
+        <div className="image">
+          <img
+            src="	https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=1390&crop=1"
+            alt=""
+          />
+        </div>
+        <div className="texts">
+          <h2>full-house battery backup coming later this year</h2>
+          <p className="info">
+            <a className="author">leo messi</a>
+            <time>2023-01-06 16:45</time>
+          </p>
+          <p className="summary">
+            Today at its special launch event, home backup power giant EcoFlow
+            launched a flurry of new products, including a “Whole-Home Backup
+            Power Solution.
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img
+            src="	https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=1390&crop=1"
+            alt=""
+          />
+        </div>
+        <div className="texts">
+          <h2>full-house battery backup coming later this year</h2>
+          <p className="info">
+            <a className="author">leo messi</a>
+            <time>2023-01-06 16:45</time>
+          </p>
+          <p className="summary">
+            Today at its special launch event, home backup power giant EcoFlow
+            launched a flurry of new products, including a “Whole-Home Backup
+            Power Solution.
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img
+            src="	https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=1390&crop=1"
+            alt=""
+          />
+        </div>
+        <div className="texts">
+          <h2>full-house battery backup coming later this year</h2>
+          <p className="info">
+            <a className="author">leo messi</a>
+            <time>2023-01-06 16:45</time>
+          </p>
+          <p className="summary">
+            Today at its special launch event, home backup power giant EcoFlow
+            launched a flurry of new products, including a “Whole-Home Backup
+            Power Solution.
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
